184414060a55360a602e907052cac130f42df790
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-04-28 22:02:22 +0200

    data-col=wrong numbering

--- components/chess/Row.tsx
+++ components/chess/Row.tsx
@@ -11,10 +11,28 @@ interface Cell{
 }
 
 export default function Row({row, indexRow}: Props) {
+  // const isPieceSelectedState = useSelector((state: RootState) =>state.moveFrom.valueSelected);
+  const handleClick = (cell: Cell, event: React.MouseEvent) => {
+    console.log('ups');
+    
+        // if(isPieceSelectedState){
+        //     getTheCellTOMove(event, cell);
+        // } else {
+        //     getImagePositionFROM(cell);
+        // }
+        
+    }
+
+    const getLetterFromIndex = (index: number): string => {
+        return String.fromCharCode(72 - index).toLowerCase();
+    }
   return (
     <div>
-        {row.map((cell, index) => (
-            <div key={index} className={index % 2 === indexRow % 2 ? "w-10 h-10 bg-white" : "w-10 h-10 bg-black"}>
+        {row.map((cell, indexColumn) => (
+            <div key={indexColumn} className={indexColumn % 2 === indexRow % 2 ? "w-10 h-10 bg-yellow-100" : "w-10 h-10 bg-orange-900"}
+            data-col={`${getLetterFromIndex(indexColumn)}${indexRow + 1}`}
+            onClick={(event) => cell && handleClick(cell, event)}
+            >
                 <Cell cell={cell}/>
             </div>
         ))}
